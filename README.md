11e0f189e267e808636770b7576c242e2b3c339c
Lasantha <hellolasantha@gmail.com>
2023-05-13 13:31:40 +0200

    :tada: Init scaffolding

---
+++ README.md
@@ -0,0 +1,11 @@
+# Heading 1
+
+Description
+
+## Heading 2
+
+Heading 2 description
+
+### Heading 3
+
+Heading 3 description
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="bg-gray-200 h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-gray-800">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
